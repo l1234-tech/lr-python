abc881cfff1fa9943a827c1d62dfc179d0657ec1
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-09-21 23:14:55 +0300

    Update test_binary_search.py

--- LR2/test_binary_search.py
+++ LR2/test_binary_search.py
@@ -1,24 +1,26 @@
-from guess_number import binary_searh
+from guess_number import binary_search
 import unittest
-
-
 class TestMySolution(unittest.TestCase):
-
     def test_simple(self):
-        self.assertEqual(binary_searh(2, list(range(1, 100)), 0), [2, 7])
+        self.assertEqual(binary_search(2, list(range(1,100)), 0), [2,7])
         # угадывание числа 2 из диапозона 1 - 100
-        self.assertEqual(binary_searh(2, list(range(0, 50)), 0), [2, 4])
-        # угадывание числа 2 из диапозона 0 - 50
-        self.assertEqual(binary_searh(2, list(range(1, 10000)), 0), [2, 12])
-        # угадывание числа 2 из диапозона 1 - 10000
-        self.assertEqual(binary_searh(-67, list(range(-100, -50)), 0), [-67, 4])
-        # угадывание числа -67 из диапозона -100 - (-50)
-        self.assertEqual(binary_searh(2, list(range(-10, 10)), 0), [2, 4])
-        # угадывание числа 2 из диапозона -10 - 10
-        self.assertEqual(binary_searh(2, list(range(3, 100)), 0), [None, 6])
-        # угадывание числа 2 из диапозона 3 - 100 (угадываемое число находится вне интервала)
-        self.assertEqual(binary_searh(1234, list(range(123, 1234567)), 0), [1234, 19])
-        # угадывание числа 1234 из диапозона 123 - 1234567
 
+        self.assertEqual(binary_search(2, list(range(0,50)), 0), [2,4])
+        # угадывание числа 2 из диапозона 0 - 50
+
+        self.assertEqual(binary_search(2, list(range(1, 10000)), 0), [2, 12])
+        # угадывание числа 2 из диапозона 1 - 10000
+
+        self.assertEqual(binary_search(-67, list(range(-100, -50)), 0), [-67, 4])
+        # угадывание числа -67 из диапозона -100 - (-50)
+
+        self.assertEqual(binary_search(2, list(range(-10, 10)), 0), [2, 4])
+        # угадывание числа 2 из диапозона -10 - 10
+
+        self.assertEqual(binary_search(2, list(range(3, 100)), 0), [None, 6])
+        # угадывание числа 2 из диапозона 3 - 100 (угадываемое число находится вне интервала)
+
+        self.assertEqual(binary_search(1234, list(range(123, 1234567)), 0), [1234, 19])
+        # угадывание числа 1234 из диапозона 123 - 1234567
 if __name__ == '__main__':
     unittest.main()
