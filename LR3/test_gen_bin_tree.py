3ac05bce5c2ccb27b4dbee3fac167068b6bc9b78
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-09-26 20:01:55 +0300

    Update test_gen_bin_tree.py

--- LR3/test_gen_bin_tree.py
+++ LR3/test_gen_bin_tree.py
@@ -1,9 +1,14 @@
-from lr3 import gen_bin_tree , left_leaf , right_leaf
+from lr3 import gen_bin_tree
 import unittest
 class TestMySolution(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(gen_bin_tree(0, 18), [18])
         # строим бинарное дерево, где высота 0, то есть должно вывести просто root
 
+        self.assertEqual(gen_bin_tree(-1, 18), "Некорректное значение height (должно быть натуралбное число или 0)")
+        # строим бинарное дерево, где высота отрицательна
+
+        self.assertEqual(gen_bin_tree(1, 18), [18, 30, 52])
+        # строим бинарное дерево, где высота больше нуля
 if __name__ == '__main__':
     unittest.main()
