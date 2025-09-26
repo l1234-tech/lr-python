87763050479095bac0efaab489c7ecadef54c5fb
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-09-26 20:17:01 +0300

    Update lr2.py

--- LR2/lr2.py
+++ LR2/lr2.py
@@ -1,6 +1,11 @@
-from guess_number import binary_searh
+from guess_number import binary_search
+
 chislo = int(input('Введите угадываемое число = '))
+
 d1 = int(input('Введите нижний порог интервала, в котором находится ваше число = '))
+
 d2 = int(input('Введите верхний порог интервала, в котором находится ваше число = '))
-list = list(range(d1, d2 + 1))
-print(binary_searh(chislo, list, 0))
+
+list = list(range(d1,d2 + 1))
+
+print(binary_search(chislo,list,0))
