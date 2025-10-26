4f07fde2a6bcd09173d7ed6479e7fbc3aea69167
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-10-26 14:09:11 +0300

    Add files via upload

---
+++ LR7/main1.py
@@ -0,0 +1,61 @@
+import requests
+import sys
+import io
+import functools
+
+
+def trace(func=None, *, handle=sys.stdout):
+    print(f"decorated func: {func}, {handle}")
+    if func is None:
+        print('func is None')
+        return lambda func: trace(func, handle=handle)
+    else:
+        print(f'{func.__name__}, {handle}')
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        handle.write(f"Using handling output\n")
+        # print(func.__name__, args, kwargs)
+        return func(*args, **kwargs)
+
+    # print('return inner')
+    return inner
+
+notstandartstream = io.StringIO()
+@trace
+def get_currencies(currency_codes:list , url:str = 'https://www.cbr-xml-daily.ru/daily_json.js' , handle = notstandartstream) -> dict:
+    """
+
+    {"GBP" : 127.4962 , "EUR" : 106,1028}
+    """
+    try:
+        print (handle)
+        # print(r.status_code)
+        response = requests.get(url)
+
+        response.raise_for_status()
+        data = response.json()
+
+        currencies = {}
+
+        # print(data)
+        if "Valute" in data:
+            for code in currency_codes:
+                if code in data["Valute"]:
+                    currencies[code] = data["Valute"][code]["Value"]
+                else:
+                    currencies[code] = f"Код валюты '{code}' не найден."
+        return currencies
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе API: {e}" , file = handle)
+        handle.write(f"Ошибка при запросе API: {e}")
+        raise requests.exceptions.RequestException('Упали с исключением')
+        # специально понимаем ошибку, чтобы понять что не так
+        # return None
+
+# if __name__ == '__main__':
+#     currency_list = ['USD', 'EUR', 'GBP' , 'JPY']
+#     currency_data = get_currencies(currency_list)
+#     res = get_currencies(currency_list)
+#     print(res)
---
+++ LR7/test_main.py
@@ -0,0 +1,67 @@
+import io
+import unittest
+import requests
+from main1 import trace
+from main1 import get_currencies
+MAX_R_VALUE = 1000
+
+class TestGetCurrencies(unittest.TestCase):
+
+
+    def test_currency_usd(self):
+      currency_list = ['USD']
+      currency_data = get_currencies(currency_list)
+
+      self.assertIn(currency_list[0], currency_data)
+      self.assertIsInstance(currency_data['USD'], float)
+      self.assertGreaterEqual(currency_data['USD'], 0)
+      self.assertLessEqual(currency_data['USD'], MAX_R_VALUE)
+
+    def test_nonexist_code(self):
+      self.assertIn("Код валюты", get_currencies(["XYZ"])["XYZ"])
+      self.assertIn("XYZ", get_currencies(["XYZ"])["XYZ"])
+      self.assertIn("не найден", get_currencies(["XYZ"])["XYZ"])
+
+    def test_get_currency_error(self):
+        error_phrase_regex = "Ошибка при запросе к API"
+        currency_list = ['USD']
+        with self.assertRaises(requests.exceptions.RequestException):
+            currency_data = get_currencies(currency_list, url="https://")
+
+# найти каким образом проверить содержание фразы erroe_phase_regex в потоке вывода
+# Дополнить тест , который должен проверять что в потоке, куда пишет функция get_currecies содержится error_phrase_regex
+# ля использования ssertStartsWith или assertRegex
+
+
+class TestStreamWrite(unittest.TestCase):
+
+
+  def setUp(self):
+    self.nonstandardstream = io.StringIO()
+    self.trace = trace(get_currencies, handle=self.nonstandardstream)
+
+
+  def broken_trace(self):
+      broken_stream = io.StringIO()
+      broken_stream.write('Broken_Info')
+      # создали заведомо сломанный поток, который заменяет изначальный, т.е. если в main был бы некорректный поток, то на тесте
+      # эта функция перехватила бы этот поток и запутила тест, который выдал бы исключение
+      with self.assertRaises(requests.exceptions.RequestException):
+          get_currencies(['USD'] , url = "https://", handle = broken_stream)
+      #     здесь мы проверяем неверный поток
+      self.assertIn("Ошибка при запросе API", broken_stream.read())
+
+
+  def test_writing_stream(self):
+    with self.assertRaises(requests.exceptions.RequestException):
+        self.trace(['USD'], url="https://")
+
+
+  def tearDown(self):
+    del self.nonstandardstream
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
