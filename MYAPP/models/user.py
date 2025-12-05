0350e9ea6c2cfb75a25fea2a9f767a1cb70be628
Andrey Syrchin <toxiclipsiz@gmail.com>
2025-12-05 22:42:11 +0300

    Update user.py

--- MYAPP/models/user.py
+++ MYAPP/models/user.py
@@ -1,7 +1,8 @@
-class User():
-    def __init__(self,id:int, name:str = 'Andrew'):
-        self.__id = id
+class User:
+    def __init__(self, user_id: int, name: str = 'Andrew'):
+        self.__id = user_id
         self.__name = name
+        self.__subscriptions = []
 
     @property
     def name(self):
@@ -19,8 +20,43 @@ class User():
         return self.__id
 
     @id.setter
-    def id(self, id):
-        if len(id) > 0 and type(id) is int:
-            self.id = id
+    def id(self, user_id):
+        if user_id > 0 and isinstance(user_id, int):
+            self.__id = user_id
         else:
             raise ValueError('ID должно быть положительным числом')
+
+    @property
+    def subscriptions(self):
+        return self.__subscriptions.copy()
+
+    def add_subscription(self, currency_code: str):
+        if currency_code not in self.__subscriptions:
+            self.__subscriptions.append(currency_code)
+            return True
+        return False
+
+    def remove_subscription(self, currency_code: str):
+        if currency_code in self.__subscriptions:
+            self.__subscriptions.remove(currency_code)
+            return True
+        return False
+
+    def has_subscription(self, currency_code: str):
+        return currency_code in self.__subscriptions
+
+    def get_subscriptions_count(self):
+        """Получить количество подписок"""
+        return len(self.__subscriptions)
+
+    def to_dict(self):
+        """Преобразовать объект в словарь для JSON"""
+        return {
+            'id': self.__id,
+            'name': self.__name,
+            'subscriptions': self.__subscriptions.copy(),
+            'subscriptions_count': len(self.__subscriptions)
+        }
+
+    def __repr__(self):
+        return f"User(id={self.__id}, name='{self.__name}', subscriptions={self.__subscriptions})"
